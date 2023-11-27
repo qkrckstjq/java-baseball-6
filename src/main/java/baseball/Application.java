183f4dbd3e8965931700017c7290aaf75609316b
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-11-27 16:26:54 +0900

    feat 프로젝트 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,7 @@
 package baseball;
 
-import baseball.util.ResetGame;
-import baseball.view.Output;
-import baseball.controller.GamePlay;
-
-
 public class Application {
     public static void main(String[] args) {
-        Output.printGameStart();
-        boolean isReset = true;
 
-        while(isReset) {
-            GamePlay.playing();
-            isReset = ResetGame.isReset();
-        }
     }
 }
